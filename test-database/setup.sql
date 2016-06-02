f36403014e3a3dbf9d0f46bd9a8002ad26a0addb
colm-mcqs <colmoshea@mcqs.com>
2016-06-02 17:21:56 +0100

    initial commit

---
+++ test-database/setup.sql
@@ -0,0 +1,6 @@
+create table directory (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email TEXT, phone_number TEXT);
+insert into directory (email, phone_number) values ('bobdole@whitehouse.us.gov', '+1 888 123 1111');  
+insert into directory (email, phone_number) values ('donaldtrump@whitehouse.us.gov', '+1 888 123 1112');  
+insert into directory (email, phone_number) values ('barackobama@whitehouse.us.gov', '+1 888 123 1113');  
+insert into directory (email, phone_number) values ('ronaldreagan@whitehouse.us.gov', '+1 888 123 1114');  
+insert into directory (email, phone_number) values ('billclinton@whitehouse.us.gov', '+1 888 123 1115');  
---
+++ test-database/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#stop the DB and remove the db container
+docker stop db && docker rm db
